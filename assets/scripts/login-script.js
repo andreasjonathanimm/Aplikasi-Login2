0c646eb70b3de992c38f746c5c71db054da4575b
Andreas Jonathan Imm <90818914+andreasjonathanimm@users.noreply.github.com>
2023-08-11 12:18:07 +0700

    Update login-script.js
    Update some typos

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -36,7 +36,7 @@ const expectedEmail = 'admin@dicoding.com';
 */
 const expectedPassword = 'superpassword';
 
-/* Comment : Menambahakan aksi klik pada button. */
+/* Comment : Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
